7ebb5c565b4cabc62686dbdd3998a499fb252e54
742481030 <59884249+742481030@users.noreply.github.com>
2020-02-07 08:07:07 +0800

    Create Layui.php

---
+++ Layui/Layui.php
@@ -0,0 +1,148 @@
+<?
+namespace core;
+class Layui{
+	public function  __construct(){
+		
+			echo "<link rel=\"stylesheet\" href=\"./layui/css/layui.css\" media=\"all\">\n";
+echo "	<script src=\"./layui/layui.js\"></script>\n";
+		
+		
+	}
+	public static function form_table_one($title,$name,$con){
+			echo ' <div class="layui-form-item">
+    <label class="layui-form-label">'.$title.'</label>
+    <div class="layui-input-block">
+      <input type="text" name="'.$name.'" lay-verify="title" autocomplete="off" placeholder="'.$con.'" class="layui-input">
+    </div>';
+		
+		
+		
+	}
+	public static function btn($name="提交",$type=""){
+		/*
+		<button type="button" class="layui-btn layui-btn-primary">原始按钮</button>
+    <button type="button" class="layui-btn">默认按钮</button>
+    <button type="button" class="layui-btn layui-btn-normal">百搭按钮</button>
+    <button type="button" class="layui-btn layui-btn-warm">暖色按钮</button>
+    <button type="button" class="layui-btn layui-btn-danger">警告按钮</button>
+    <button type="button" class="layui-btn layui-btn-disabled">禁用按钮</button>
+		
+		
+		
+		
+		
+		*/
+		
+		return '<button type="button" class="layui-btn '.$type.'">'.$name.'</button>';
+	}
+	public static function form_table_selete(){
+		
+	}
+	public static function form_table_search(){
+		
+	}
+	public static function form_checkbox(){
+		
+	}
+	public static function form_close(){
+		
+	}
+	public static function form_radio(){
+		
+	}
+	public static function form_set(){
+		
+	}
+	public static function form_get(){
+		
+	}
+	public static function nav_level(){
+		
+	}
+	public static function nav_vertical(){
+		
+	}
+	public static function tap(){
+		
+	}
+	public static function tab_hash(){
+		
+	}
+	public static function tab_card(){
+		
+	}
+	public static function tab_new(){
+		
+	}
+	public static function tab_del(){
+		
+	}
+	public static function progress(){
+		
+	}
+	public static function card(){
+		
+	}
+	public static function badge(){
+		
+	}
+	public static function timeline(){
+		
+	}
+	public static function table(){
+		
+	}
+	public static function flash(){
+		
+	}
+	public static function quote(){
+		
+	}
+	public static function tip(){
+		
+	}
+	public static function date(){
+		
+	}
+	public static function table_new(){
+		
+	}
+	public static function pre(){
+		
+	}
+	public static function upload(){
+		
+	}
+	public static function slider(){
+		
+	}
+	public static function rate(){
+		
+	}
+	public static function flow(){
+		
+	}
+	public static function layer(){
+		
+	}
+	public static function code(){
+		
+	}
+	public static function qeeeeeeqq(){
+		
+	}
+	public static function qeeeeeqq(){
+		
+	}
+	public static function qqeeeeeeq(){
+		
+	}
+	public static function qqeeeeeeeq(){
+		
+	}
+	public static function qqeeeq(){
+		
+	}
+
+	
+}
